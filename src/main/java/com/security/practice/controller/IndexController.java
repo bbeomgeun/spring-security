1345f144601b9046a439686bcd7581b8c09d16c8
bbeomgeun <miki308@naver.com>
2022-02-04 00:57:42 +0900

    refactor: view 반환을 위해 controller로 변경

--- src/main/java/com/security/practice/controller/IndexController.java
+++ src/main/java/com/security/practice/controller/IndexController.java
@@ -1,13 +1,15 @@
 package com.security.practice.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
+        // 머스테치 기본 폴더 src/main/resources/
+        // 뷰 리졸버 : templates (prefix), .mustache (suffix)
         return "index";
     }
 }
