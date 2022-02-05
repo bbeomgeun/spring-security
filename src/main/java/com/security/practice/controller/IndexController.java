783c18f46dd96cf769f2dc59a8190bce1e615266
bbeomgeun <miki308@naver.com>
2022-02-05 22:44:58 +0900

    feat: IndexController  기능 추가

--- src/main/java/com/security/practice/controller/IndexController.java
+++ src/main/java/com/security/practice/controller/IndexController.java
@@ -12,4 +12,35 @@ public class IndexController {
         // 뷰 리졸버 : templates (prefix), .mustache (suffix)
         return "index";
     }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 해당 주소를 낚아챔 -> SecurityConfig 파일 생성 후 작동 안함.
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public String joinProc() {
+        return "회원가입 완료!";
+    }
 }
